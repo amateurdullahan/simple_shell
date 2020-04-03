92935d4be1a4816207e5b0c917a12b9874001ec1
Sean Taylor <silverwolf4350@gmail.com>
2020-04-03 20:57:10 +0000

    Handle arguments.

--- sholby.c
+++ sholby.c
@@ -3,14 +3,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <sys/types.h>
+#include <string.h>
 
 int main(int argc, char **argv, char **env)
 {
 	int i = 0, res, cpid;
 	size_t buffsize = 1;
-	char *buff;
-	char *sargs;
-	const char s = '-';
+	char *buff, **sargs;
+	const char *s = " ";
 
 	while (1)
 	{
@@ -33,11 +33,15 @@ int main(int argc, char **argv, char **env)
 			while (buff[i] != '\n' && buff[i] != '\0')
 				i++;
 			buff[i] = '\0';
-			sargs = strtok(buff, s);
-			res = execve(sargs, argv, env);
+			sargs = malloc(100);
+			sargs[0] = strtok(buff, s);
+			for (i = 0; sargs[i] != NULL; i++)
+				sargs[i + 1] = strtok(NULL, s);
+			res = execve(sargs[0], sargs, env);
 			if (res == -1)
-				printf("%s: 1: %s does not exist\n", argv[0], buff);
+				printf("%s: 1: %s does not exist\n", argv[0], sargs[0]);
 			free(buff);
+			free(sargs);
 			break;
 		}
 		else
