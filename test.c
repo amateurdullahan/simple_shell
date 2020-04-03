23dff2b96e04660f5ac32cf52e6b982964124d65
amateurdullahan <zabrak12@gmail.com>
2020-04-03 02:17:45 +0000

    commit to checkout

--- test.c
+++ test.c
@@ -7,24 +7,28 @@ int main(int argc, char **argv, char **env)
 	int res = 0;
 	size_t buffsize = 1;
 	char *buff = malloc(sizeof(char) * buffsize);
-
+	
 	if (buff == NULL)
 	{
 		printf("Failed to malloc.\n");
 		return (1);
 	}
+	fork();
+	while (1)
+	  {
+	    wait();
+	    printf("$ ");
+	    
+	    getline(&buff, &buffsize, stdin);
 
-	printf("$ ");
 
-	getline(&buff, &buffsize, stdin);
+	    buff[buffsize - 2] = '\0';
 
+	    res = execve(buff, argv, env);
 
-	buff[buffsize - 2] = '\0';
-
-	res = execve(buff, argv, env);
-
-	if (res == -1)
+	    if (res == -1)
 		printf("%s: 1: %s does not exist\n", argv[0], buff);
-
+    
+	  }
 	return (0);
 }
