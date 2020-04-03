a58b00f12860e52fe57cbd1d8a85782ed068e091
Sean Taylor <silverwolf4350@gmail.com>
2020-04-03 17:25:36 +0000

    Trying to fix pipe loop.

--- sholby.c
+++ sholby.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <fcntl.h>
 #include <sys/wait.h>
 #include <sys/types.h>
 
 int main(int argc, char **argv, char **env)
 {
-	int res, cpid;
+	int i = 0, res, cpid;
 	size_t buffsize = 1;
 	char *buff;
 
 	while (1)
 	{
-		cpid = fork()
+		cpid = fork();
 		if (cpid == -1)
 		{
 			printf("Fork failed");
@@ -20,23 +21,31 @@ int main(int argc, char **argv, char **env)
 		}
 		else if (cpid == 0)
 		{
-		  buff = malloc(sizeof(char) * buffsize);
-		  if (buff == NULL)
-		    {
-		      printf("Failed to malloc.\n");
-		      return (1);
-		    }
-		  printf("$ ");
-		  getline(&buff, &buffsize, stdin);
-		  buff[buffsize - 2] = '\0';
-		  res = execve(buff, argv, env);
-		  if (res == -1)
-		    printf("%s: 1: %s does not exist\n", argv[0], buff);
-		  free(buff);
-		  break;
+			buff = malloc(sizeof(char) * buffsize);
+			if (buff == NULL)
+			{
+				printf("Failed to malloc.\n");
+				return (1);
+			}
+			printf("$ ");
+			getline(&buff, &buffsize, stdin);
+			if (buff[0] == '\0')
+				printf("\nNull char\n");
+			else
+				printf("\nNot null char\n");
+			while (buff[i] != '\n' && buff[i] != '\0')
+				i++;
+			buff[i] = '\0';
+			res = execve(buff, argv, env);
+			if (res == -1)
+				printf("%s: 1: %s does not exist\n", argv[0], buff);
+			free(buff);
+			break;
 		}
 		else
-		  wait(NULL);
+		{
+			wait(NULL);
+		}
 	}
 	return (0);
 }
