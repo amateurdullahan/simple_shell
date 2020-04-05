c3597a759a5b2bc2e679d77ad064b8af75171cd0
amateurdullahan <zabrak12@gmail.com>
2020-04-05 20:40:03 +0000

    add header

--- holberton.h
+++ holberton.h
@@ -1,6 +1,15 @@
 #ifndef HOLBERTON_H
 #define HOLBERTON_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <string.h>
+
+
+
 char **tokenize(char *buff);
 
 
