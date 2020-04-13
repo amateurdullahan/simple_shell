4becba32750edc5c38550ed6582b1dc3743ab889
amateurdullahan <zabrak12@gmail.com>
2020-04-13 20:07:23 +0000

    add _printf

--- holberton.h
+++ holberton.h
@@ -8,7 +8,7 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <string.h>
-
+#include <stdarg.h>
 
 
 char **tokenize(char *buff);
@@ -22,5 +22,18 @@ int chexe(char *cmd, char **sargs, char **env);
 void exitbltin(char *buff);
 void envbltin(char *buff, char **env);
 char *prepbuff();
+
+/* PRINTF FUNCTIONS */
+
+int _printf(const char *format, ...);
+void _puts(char *str);
+int _putchar(char c);
+char *_itoa(int value, char *buffer, int base);
+int _abs(int a);
+char *_reverse(char *buffer, int i, int j);
+void _swap(char *x, char *y);
+int print_conv(const char *str, va_list arg);
+int print_s(const char *str, va_list arg);
+
 
 #endif /* HOLBERTON_H */
